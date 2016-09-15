a4be8bf6e34f4414e4300e3647fbc423fa4a6ca2
wangyq <yinqingwang@gmail.com>
2016-09-15 10:30:53 +0800

    first version

---
+++ HelloWebJava/src/com/siwind/web/HelloEndPoint.java
@@ -0,0 +1,55 @@
+package com.siwind.web;
+
+import java.io.IOException;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @ServerEndpoint 注解是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端,
+ * 注解的值将被用于监听用户连接的终端访问URL地址,客户端可以通过这个URL来连接到WebSocket服务器端
+ */
+@ServerEndpoint("/WSHello")
+public class HelloEndPoint {
+	/**
+     * @OnOpen allows us to intercept the creation of a new session.
+     * The session class allows us to send data to the user.
+     * In the method onOpen, we'll let the user know that the handshake was 
+     * successful.
+     */
+    @OnOpen
+    public void onOpen(Session session){
+        System.out.println("Session " + session.getId() + " has opened a connection"); 
+        try {
+            session.getBasicRemote().sendText("Connection Established");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+ 
+    /**
+     * When a user sends a message to the server, this method will intercept the message
+     * and allow us to react to it. For now the message is read as a String.
+     */
+    @OnMessage
+    public void onMessage(String message, Session session){
+        System.out.println("Message from " + session.getId() + ": " + message);
+       
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+ 
+    /**
+     * The user closes the connection.
+     * 
+     * Note: you can't send messages to the client from this method
+     */
+    @OnClose
+    public void onClose(Session session){
+        System.out.println("Session " +session.getId()+" has closed!");
+    }
+    
+}
